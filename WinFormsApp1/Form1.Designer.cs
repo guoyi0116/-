7cc9ba0984d76be6d3449632ac2340d8dc3e1ba5
user <user@A401-PC51>
2022-04-27 17:17:57 +0800

    加入專案檔案。

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btntruegray = new System.Windows.Forms.Button();
+            this.btnLord2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.butngray = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.btnlightup = new System.Windows.Forms.Button();
+            this.marcycar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btntruegray
+            // 
+            this.btntruegray.Location = new System.Drawing.Point(156, 311);
+            this.btntruegray.Name = "btntruegray";
+            this.btntruegray.Size = new System.Drawing.Size(147, 64);
+            this.btntruegray.TabIndex = 0;
+            this.btntruegray.Text = "灰階";
+            this.btntruegray.UseVisualStyleBackColor = true;
+            this.btntruegray.Click += new System.EventHandler(this.buttonlord_Click);
+            // 
+            // btnLord2
+            // 
+            this.btnLord2.Location = new System.Drawing.Point(3, 311);
+            this.btnLord2.Name = "btnLord2";
+            this.btnLord2.Size = new System.Drawing.Size(147, 64);
+            this.btnLord2.TabIndex = 2;
+            this.btnLord2.Text = "載入圖片";
+            this.btnLord2.UseVisualStyleBackColor = true;
+            this.btnLord2.Click += new System.EventHandler(this.btnLord2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(12, 41);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(218, 264);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // butngray
+            // 
+            this.butngray.Location = new System.Drawing.Point(309, 311);
+            this.butngray.Name = "butngray";
+            this.butngray.Size = new System.Drawing.Size(147, 64);
+            this.butngray.TabIndex = 4;
+            this.butngray.Text = "負片";
+            this.butngray.UseVisualStyleBackColor = true;
+            this.butngray.Click += new System.EventHandler(this.butngray_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(258, 41);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(218, 264);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // btnlightup
+            // 
+            this.btnlightup.Location = new System.Drawing.Point(472, 311);
+            this.btnlightup.Name = "btnlightup";
+            this.btnlightup.Size = new System.Drawing.Size(147, 64);
+            this.btnlightup.TabIndex = 6;
+            this.btnlightup.Text = "變亮";
+            this.btnlightup.UseVisualStyleBackColor = true;
+            this.btnlightup.Click += new System.EventHandler(this.btnlightup_Click);
+            // 
+            // marcycar
+            // 
+            this.marcycar.Location = new System.Drawing.Point(625, 311);
+            this.marcycar.Name = "marcycar";
+            this.marcycar.Size = new System.Drawing.Size(147, 64);
+            this.marcycar.TabIndex = 7;
+            this.marcycar.Text = "馬賽克";
+            this.marcycar.UseVisualStyleBackColor = true;
+            this.marcycar.Click += new System.EventHandler(this.marcycar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.marcycar);
+            this.Controls.Add(this.btnlightup);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.butngray);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btnLord2);
+            this.Controls.Add(this.btntruegray);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btntruegray;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnLord2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button butngray;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button btnlightup;
+        private System.Windows.Forms.Button marcycar;
+    }
+}
+
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonlord_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = null;
+            int h, w;
+            bmp = new Bitmap(pictureBox2.Image);
+            w = bmp.Width;
+            h = bmp.Height;
+            for (int y = 0; y < h; y++)
+            {
+                for (int x = 0; x < w; x++)
+                {
+                    Color p = bmp.GetPixel(x, y);
+                    int avg = (int)(p.R * 0.3 + p.G * 0.6 + p.B * 0.1);
+                    bmp.SetPixel(x, y, Color.FromArgb(p.A, avg, avg, avg));
+                }
+            }
+
+            pictureBox3.Image = bmp;
+        }
+
+        private void btnLord2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dia = new OpenFileDialog();
+            dia.Title = "請選擇圖形檔";
+            dia.Filter = "jpeg files|*.jpg";
+            if (dia.ShowDialog() == DialogResult.OK) ;
+            {
+               pictureBox2.Load(dia.FileName);
+            }
+        }
+
+        private void butngray_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = null;
+            int h, w;
+            bmp = new Bitmap(pictureBox2.Image);
+            w = bmp.Width;
+            h = bmp.Height;
+            for(int y = 0; y < h; y++)
+            {
+                for(int x = 0; x <w; x++)
+                {
+                    Color p = bmp.GetPixel(x, y);
+                    bmp.SetPixel(x, y, Color.FromArgb(p.A, 255 - p.R, 255 - p.G, 255 - p.B));
+                }
+            }
+         
+            pictureBox3.Image = bmp;
+        }
+
+        private void btnlightup_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = null;
+            int h, w;
+            bmp = new Bitmap(pictureBox2.Image);
+            w = bmp.Width;
+            h = bmp.Height;
+            for (int y = 0; y < h; y++)
+            {
+                for (int x = 0; x < w; x++)
+                {
+                    Color p = bmp.GetPixel(x, y);
+                    int r = (int)(p.R * 1.2 > 255 ? 255 : p.R * 1.2);
+                    int b = (int)(p.B * 1.2 > 255 ? 255 : p.B * 1.2);
+                    int g = (int)(p.G * 1.2 > 255 ? 255 : p.G * 1.2);
+
+
+                    bmp.SetPixel(x, y, Color.FromArgb(p.A, r, b, g));
+                }
+            }
+
+            pictureBox3.Image = bmp;
+        }
+
+        private void marcycar_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = null;
+            int h, w;
+            bmp = new Bitmap(pictureBox2.Image);
+            w = bmp.Width;
+            h = bmp.Height;
+            const int size = 20;
+
+            for (int y = 0; y < h; y=size   )
+            {
+                for (int x = 0; x < w; x=size)
+                {
+                    Color p = bmp.GetPixel(x, y);
+                    for(int i = 0; i < size; i++)
+                    {
+                        for(int j = 0; j < size; j++)
+                        {
+                            bmp.SetPixel(x + i, y + j, p);
+                        }
+                            
+                        
+                    }
+
+
+                   
+                }
+            }
+
+            pictureBox3.Image = bmp;
+        }
+    }
+}
